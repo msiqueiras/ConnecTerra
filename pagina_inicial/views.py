f0067c50e702ee8e12d32112650aa390be87f27f
msiqueiras <devef.msiqueira@gmail.com>
2026-01-03 03:03:28 -0300

    :sparkles: style: adição do front-end da seção sobre do homepage

--- pagina_inicial/views.py
+++ pagina_inicial/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render #shortcuts = atalhos
 
 def pagina_inicial(request):
     return render(request, 'pagina_inicial.html')
+
+def sobre(request):
+    return render(request, 'sobre.html' )
+
+def nossos_contatos(request):
+    return render(request, 'nossos_contatos.html')
