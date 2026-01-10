424936a8756d0cefbf7a0e6569760752d1aaed73
msiqueiras <devef.msiqueira@gmail.com>
2026-01-10 00:59:34 -0300

    sytle: adição das páginas 'sobre' e 'nossos contatos'

--- pagina_inicial/urls.py
+++ pagina_inicial/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-		path('', views.pagina_inicial, name='pagina_inicial')
+		path('', views.pagina_inicial, name='pagina_inicial'),
+        path('sobre/', views.sobre, name='sobre'),
+        path('nossoscontatos/', views.nossos_contatos, name='nossos_contatos')
 ]
