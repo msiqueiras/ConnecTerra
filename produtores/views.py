2a222889e15d794ed332338326b0d0522e6220c2
msiqueiras <devef.msiqueira@gmail.com>
2026-01-12 21:40:02 -0300

    feat: filtro de busca

--- produtores/views.py
+++ produtores/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from produtores.models import ProdutoresRurais
 
 def produtoresrurais(request):
-    produtores = ProdutoresRurais.objects.all()
+    produtores = ProdutoresRurais.objects.all().order_by('full_name')
+    #parametros de pesquisa
+    search = request.GET.get('search')
+    if search == None:
+        search = ''
+    produtores = ProdutoresRurais.objects.filter(city__icontains=search)
 
     return render(request,'catalago_produtores.html',{'produtores': produtores})
