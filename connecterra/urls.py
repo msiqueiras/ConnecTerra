adcd3d6c704945352fa3ffc0627f926c5720dbb1
ClaudioHNQ <contatohenrique97@gmail.com>
2026-01-16 03:32:42 -0300

    refactor UI/UX, new logo and fixed routing
    * Refatoração UI/UX:
    - nova logo
    - redesign da seção hero
    - criação de um footer
    - centralização global de estilos
    - padronização de links
    
    *correções de back:
    - import de views
    - sincronização de arquivos).

--- connecterra/urls.py
+++ connecterra/urls.py
@@ -1,22 +1,3 @@
-"""
-URL configuration for connecterra project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a UR<a \href = "{% url 'pagina_inicial' %}" class="illustration-right">
-                <img src = "{% static 'images/celeiro.png' %}" width = '500' height = '300' alt = 'Celeiro de fazenda'>
-            </a>
-L to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,17 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pagina_inicial.urls')),
+    path('', include('pagina_inicial.urls')), 
     path('selosverdes/', include('selosverdes.urls')),
-    path('produtores/', include('produtores.urls'))
-
+    path('produtores/', include('produtores.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
- 
--- pagina_inicial/urls.py
+++ pagina_inicial/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 		path('', views.pagina_inicial, name='pagina_inicial'),
         path('sobre/', views.sobre, name='sobre'),
-        path('nossoscontatos/', views.nossos_contatos, name='nossos_contatos')
+        path('contatos/', views.nossos_contatos, name='nossoscontatos'),
 ]
